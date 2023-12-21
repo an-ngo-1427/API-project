96cd65cc350054cc6815ab129c2947f1efb7b45d
An Ngo <anng0149@gmail.com>
2023-12-21 10:38:38 -0800

    add signup valdiation to signup api

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,7 +7,35 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-router.post('/',async (req,res)=>{
+const {check} = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+
+// validate signup middleware
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+
+// sign up end point
+router.post('/',validateSignup,async (req,res)=>{
     const {email,username,password} = req.body;
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({
