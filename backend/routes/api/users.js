a9cc34b3b66d989b8afe102eb63fe2a758bff2ea
An Ngo <anng0149@gmail.com>
2023-12-21 09:25:04 -0800

    add user login api

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,30 @@
 const express = require('express')
 const router = express.Router();
 
+
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.post('/',async (req,res)=>{
+    const {email,username,password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+        email,
+        username,
+        hashedPassword
+    })
+
+    const safeUser = {
+        id:user.id,
+        username:user.username,
+        email:user.email
+    }
+
+    await setTokenCookie(res,safeUser);
+    res.json({
+        user:safeUser
+    })
+})
 module.exports = router;
